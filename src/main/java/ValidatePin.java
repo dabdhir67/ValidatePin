29189000303ca9e57a6e57e561b3eea782f60e8e
David Abdhir <dabdhir67@gmail.com>
2022-01-28 07:55:15 -0800

    second committ

--- src/main/java/ValidatePin.java
+++ src/main/java/ValidatePin.java
@@ -6,12 +6,12 @@ public class ValidatePin {
     public static void main(String[] args) {
         List<String> pins = new ArrayList<>(Arrays.asList("121317", "1234", "45135", "89abc1", "900876", " 4983", ""));
         for(String pin : pins) {
-            System.out.println(pin + "is a " + validate(pin) + " pin");
+            System.out.println(pin + "---->" + "is a " + isValid(pin) + " pin");
         }
 
     }
 
-    private static boolean validate(String pin) {
+    private static boolean isValid(String pin) {
         if((pin.length()!=4)&&(pin.length()!=6)||!pin.matches("[0-9]+")) {
             return false;
         }else {
